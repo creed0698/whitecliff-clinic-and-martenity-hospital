1befc6d4f86078a56c7ace6eb86c9a3c30e9e2b7
Elton Chipuka <eltonchipuka@gmail.com>
2022-08-26 11:17:03 +0200

    db conn commit

--- inc/db.php
+++ inc/db.php
@@ -1,4 +1,10 @@
 <?php
+
+$server = ";"
+$username = ";"
+$password = ";"
+$dbname = ";"
+
 $con=mysqli_connect("localhost", "root", "", "whitecliff");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
